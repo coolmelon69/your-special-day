94d2cd86d66ed4cd8473d5dd56a39a0a62f25e2b
adam.danial <coolmelon69@gmail.com>
2026-03-13 16:01:40 +0800

    latest

--- ADD_STAMP_ORDER_COLUMN.sql
+++
@@ -1,4 +0,0 @@
--- Add stamp_order column to admin_settings for drag-reorder persistence
--- Run in Supabase SQL Editor if you use the admin_settings table
-
-ALTER TABLE admin_settings ADD COLUMN IF NOT EXISTS stamp_order TEXT[] DEFAULT '{}';
